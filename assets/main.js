9be06a3a7a808b16dc3f15364faed873ed707c62
beaug <beaug@sfu.ca>
2017-08-20 23:52:04 -0700

    Done!

--- assets/main.js
+++ assets/main.js
@@ -3,7 +3,33 @@ $.ajax({
   url: 'https://www.codeschool.com/users/3871087.json',
   dataType: 'jsonp',
   success: function(response){
-    console.log('response', response);
+    console.log('response', response.courses.completed);
+    showCourses(response.courses.completed);
   }
   });
+
+   function showCourses(courses){
+     var $badges = $('#badges');
+
+     courses.forEach(function(course){
+       var $course = $('<div />', {
+         'class': 'course'
+       }).appendTo($badges);
+
+       $('<h3 />', {
+         text: course.title
+       }).appendTo($course);
+
+       $('<img />',{
+         src: course.badge
+       }).appendTo($course);
+
+       $('<a />',{
+         'class': 'btn btn-primary',
+         target: '_blank',
+         href: course.url,
+         text: 'See Course',
+       }).appendTo($course);
+     })
+   }
 });
